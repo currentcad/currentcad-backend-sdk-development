ddc65c3c5680de5df53a5781e739999cd22008bf
DESKTOP-ALVMSLF\hp <yaoyao>
2023-10-12 15:05:41 +0800

    feat:修复sdk出现的一些bug sdk使用示例

--- src/main/java/com/currentcad/sdk/export/ExportFile.java
+++ src/main/java/com/currentcad/sdk/export/ExportFile.java
@@ -11,16 +11,17 @@ public class ExportFile {
     public static void main(String[] args) {
         SDK sdk = new SDK();
         //由公司颁发的账号密码
-        sdk.setUsername("xxxxxxxxx");
-        sdk.setPassword("xxxxxx");
+//        sdk.setUsername("yangsenxiang@currentcad.com");
+//        sdk.setPassword("aA123456");
         //请求参数为图纸id， 导出图纸存放的父级目录：C:\\file
         //导出dwg
-        File dwg = sdk.exportDwg("xxxxx", "xxxxxxx");
-        //导出pdf
-        File pdf = sdk.exportPdf("xxxxx", "xxxx");
-        //导出svg
-        File svg = sdk.exportSvg("xxxx", "xxxxx");
-        //导出png
-        File png = sdk.exportPng("xxxx", "xxxxx");
+        File dwg = sdk.exportDwg("3:8312a695f12c4d989192d7793f2b741e", "D:\\file-test");
+        System.out.println(dwg.getName());
+//        //导出pdf
+//        File pdf = sdk.exportPdf("xxxxx", "xxxx");
+//        //导出svg
+//        File svg = sdk.exportSvg("xxxx", "xxxxx");
+//        //导出png
+//        File png = sdk.exportPng("xxxx", "xxxxx");
     }
 }
